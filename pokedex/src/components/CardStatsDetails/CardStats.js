81ffef8a24d8d81032e0fb62397dd5a875576286
Byron Belizario Smith Nobrega <byronnobrega@hotmail.com>
2022-09-04 10:33:09 -0300

    adicinar regra home

--- pokedex/src/components/CardStatsDetails/CardStats.js
+++ pokedex/src/components/CardStatsDetails/CardStats.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ContextDetails } from "../../context/Context";
 import { ContainerStats, InformationStats } from "./styleCardStats";
 
 
-function CardStats() {
-
-
-
+function CardStats(props) {
+   /*  const detailscontext=useContext(ContextDetails)
+    console.log(detailscontext.detailsPokemo); */
+   /*  const ListStats =
+    props.dataPokemom
+    &&props.dataPokemom.stats.map((item) => {
+      return (
+        <label key={item.id}>{item.name}</label>
+      );
+    }); */
+    console.log(props.dataPokemom.stats);
     return(
         <ContainerStats>
             <h2>Stats</h2>
             <InformationStats>
+               {/*  {props.dataPokemom&&ListStats} */}
             <label>HP:46</label>
             <label>Attack:39</label>
             <label>Defense:52</label>
--- pokedex/src/pages/Details/details.js
+++ pokedex/src/pages/Details/details.js
@@ -1,26 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import CarImages from "../../components/CardImagesDetails/CardImages";
 import CardMoves from "../../components/CardMovesDetails/CardMoves";
 import CardStats from "../../components/CardStatsDetails/CardStats";
 import Headers from "../../components/Headers/Headers";
 import { BASE_URL } from "../../Constants/Constants";
-import { Context } from "../../context/Context";
+import { Context, ContextDetails } from "../../context/Context";
 import useRequestData from "../../hooks/useRequestData";
 import { ContainerBody } from "./style";
 
 function Details() {
     const title = "Nome do Pokemom"
-    const pathParams = useParams();  
-    const carcontext=useContext(Context)
+    const pathParams = useParams();
+    const [detailsPokemo, setdetailsPokemo]=useState(); 
+    /* const detailscontext=useContext(ContextDetails) */
     const [data, isLoading, erro, reload, setReload] =
     useRequestData(`${BASE_URL}`+1);
+    
 
     console.log(`${BASE_URL}`+1);
     console.log(data);
    
     return(
-        <>
+        <ContextDetails.Provider value={{detailsPokemo,setdetailsPokemo}}>
         <Headers
         title={!isLoading&&data&&data.name}
         />
@@ -29,7 +31,7 @@ function Details() {
         <CardStats dataPokemom={data}/>
         <CardMoves dataPokemom={data}/>
         </ContainerBody>
-        </>
+        </ContextDetails.Provider>
 
     )
 
