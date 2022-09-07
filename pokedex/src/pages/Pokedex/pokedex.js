3ac1752829d1aca6e4a95553f714eb98b7c1f19a
Luana Barbosa de Oliveira <deoliveiraluana29@gmail.com>
2022-09-06 22:45:41 -0300

    pokedexpag

--- pokedex/src/pages/Pokedex/pokedex.js
+++ pokedex/src/pages/Pokedex/pokedex.js
@@ -3,14 +3,33 @@ import CardPokemon from "../../components/CardPokemon/CardPokemon";
 import Headers from "../../components/Headers/Headers";
 import { BASE_URL } from "../../Constants/Constants";
 import useRequestData from "../../hooks/useRequestData";
-import { StyleHome } from "./style";
 import { Background } from "./style";
 
-function Pokedex () {
-    return(
-        <Background>
-            <h1>Pokedex</h1>
-        </Background>
-    )
+function Pokedex() {
+  const title = "Pokedex";
+  const [data, isLoading, erro, reload, setReload] = useRequestData(
+    `${BASE_URL}`
+  );
+  console.log(data);
+  const ListPkemom =
+    !isLoading &&
+    data &&
+    data.results.map((item) => {
+      return (
+        <CardPokemon key={item.id} namePokemom={item.name} url={item.url} />
+      );
+    });
+
+  return (
+    <>
+      <Headers title={title} />
+      <Background>
+        {isLoading && <h3>Carregando...</h3>}
+        {!isLoading && data && ListPkemom}
+        {!isLoading && !data && erro}
+      </Background>
+    </>
+  );
 }
+
 export default Pokedex;
