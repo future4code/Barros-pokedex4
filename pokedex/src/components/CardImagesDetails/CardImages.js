596a11a46d3f41ab857ecf1bf5ba45954ec34404
Byron Belizario Smith Nobrega <byronnobrega@hotmail.com>
2022-09-08 09:51:43 -0300

    Adicionar inamge-Back details

--- pokedex/src/components/CardImagesDetails/CardImages.js
+++ pokedex/src/components/CardImagesDetails/CardImages.js
@@ -2,12 +2,12 @@ import React from "react";
 import { ImageCardDetails } from "./styleCardImages";
 
 function CarImages(props) {
-  console.log(props.dataPokemom.sprites.other);
+  console.log(props.dataPokemom.sprites);
   return (
     <div>
       <ImageCardDetails>
         <img src={props.dataPokemom.sprites.other.dream_world.front_default} alt="pokemom" />
-        <img src={props.dataPokemom.sprites.other.home.front_default} alt="pokemom" />
+        <img src={props.dataPokemom.sprites.back_shiny} alt="pokemom" />
       </ImageCardDetails>
     </div>
   );
