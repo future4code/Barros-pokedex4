051f5b0a0021a03ad16f1c4f72871c3b8e5109ea
Byron Belizario Smith Nobrega <byronnobrega@hotmail.com>
2022-09-08 09:31:21 -0300

    Adicionar inamge details

--- pokedex/src/components/CardImagesDetails/CardImages.js
+++ pokedex/src/components/CardImagesDetails/CardImages.js
@@ -2,11 +2,12 @@ import React from "react";
 import { ImageCardDetails } from "./styleCardImages";
 
 function CarImages(props) {
+  console.log(props.dataPokemom.sprites.other);
   return (
     <div>
       <ImageCardDetails>
-        <img src={props.dataPokemom.sprites.front_default} alt="pokemom" />
-        <img src={props.dataPokemom.sprites.back_default} alt="pokemom" />
+        <img src={props.dataPokemom.sprites.other.dream_world.front_default} alt="pokemom" />
+        <img src={props.dataPokemom.sprites.other.home.front_default} alt="pokemom" />
       </ImageCardDetails>
     </div>
   );
