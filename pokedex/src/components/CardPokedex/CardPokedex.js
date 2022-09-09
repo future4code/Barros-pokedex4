bd67373802a3a90e0f2d6a9e59f1f1373411f3df
Luana Barbosa de Oliveira <deoliveiraluana29@gmail.com>
2022-09-09 20:16:34 -0300

    cardpokedex e ajuste logica

--- pokedex/src/components/CardPokedex/CardPokedex.js
+++ pokedex/src/components/CardPokedex/CardPokedex.js
@@ -1,48 +1,17 @@
-import React,{ useContext, useState } from "react";
-import { StyleCard } from "../CardPokemon/style"
-import { Context } from "../../context/Context"
-import useRequestData from "../../hooks/useRequestData";
-import { useNavigate } from "react-router-dom";
-import { StyleHome } from "../../pages/Home/style";
+import React from "react";
+import { StyleCard } from "../CardPokemon/style";
 
+function CardPokedex(props) {
+    
+  return (
+    <StyleCard>
+      <img src={props.url} alt="Imagem do Pokemon"></img>
+      <p>props.namePokemom</p>
+      {props.buttonRemove}
+      {props.buttonView}
+      
+    </StyleCard>
+  );
+}
 
-function CardPokedex ({newListPokemons,addPokedex}) {
-    const navigate = useNavigate()
-
-    const detailsPage = (id) => {
-        navigate("/details/" + id);
-      };
-
-    const listPokemon = JSON.parse(localStorage.getItem("homePokemons"))
-
-     return(
-        <>
-        {listPokemon !== null && listPokemon.length > 0 ? listPokemon.map((poke,index) => {
-            return ( 
-                <StyleHome
-                key={poke.name}
-                style={poke.types[0].type.name === "grass" ? {backgroundImage: 'linear-gradient(to bottom  , #84ed6829 20%, #49b675 60%, #005221)'} :
-                  poke.types[0].type.name === "fire" ? {backgroundImage: 'linear-gradient(to bottom , #ff7f0099 20%, yellow 60%, #ff7f00)'} :
-                  poke.types[0].type.name === "water" ? {backgroundImage: 'linear-gradient(to bottom , #85e8ff29 10%, #0d67d2 70%, #4354e9)'} :
-                  poke.types[0].type.name === "normal" ? {backgroundImage: 'linear-gradient(to bottom , #fcf5d629 10%, #b5840d 70%, #000)'}: 
-                  poke.types[0].type.name === "bug" ? {backgroundImage: 'linear-gradient(to bottom , #78866b29 10%, #fcf5d6 70%, #78866b )'}:{}
-              }>
-              </StyleHome> 
-            )}) 
-           : newListPokemons && newListPokemons.map((poke,index)=>{
-            return (
-              <StyleHome
-                key={poke.name}
-                style={poke.types[0].type.name === "grass" ? {backgroundImage: 'linear-gradient(to bottom  , #84ed6829 20%, #49b675 60%, #005221)'} :
-                  poke.types[0].type.name === "fire" ? {backgroundImage: 'linear-gradient(to bottom , #ff7f0099 20%, yellow 60%, #ff7f00)'} :
-                  poke.types[0].type.name === "water" ? {backgroundImage: 'linear-gradient(to bottom , #85e8ff29 10%, #0d67d2 70%, #4354e9)'} :
-                  poke.types[0].type.name === "normal" ? {backgroundImage: 'linear-gradient(to bottom , #fcf5d629 10%, #b5840d 70%, #000)'}: 
-                  poke.types[0].type.name === "bug" ? {backgroundImage: 'linear-gradient(to bottom , #78866b29 10%, #fcf5d6 70%, #78866b )'}:{}
-              }>
-              </StyleHome> 
-            )})}
-          </>
-        )
-      }
-
-      export default CardPokedex;
+export default CardPokedex;
--- pokedex/src/pages/Pokedex/pokedex.js
+++ pokedex/src/pages/Pokedex/pokedex.js
@@ -1,87 +1,48 @@
-import React, { useEffect, useState } from "react";
-import CardPokemon from "../../components/CardPokemon/CardPokemon";
-import CarImages from "../../components/CardImagesDetails/CardImages";
-import CardMoves from "../../components/CardMovesDetails/CardMoves";
-import CardStats from "../../components/CardStatsDetails/CardStats";
+import React, { useContext } from "react";
 import CardPokedex from "../../components/CardPokedex/CardPokedex";
 import Headers from "../../components/Headers/headerspoke";
 import { useNavigate } from "react-router-dom";
-import { BASE_URL } from "../../Constants/Constants";
-import useRequestData from "../../hooks/useRequestData";
-import * as MyRouters from "../../Rotas/Coodinator"
 import { Background } from "./style";
-import { StyleHome } from "../Home/style";
-import { StyleCard } from "../../components/CardPokemon/style";
+import { GlobalStateContext } from "../../context/GlobalStateContext";
+
+
 
 function Pokedex() {
   const title = "Pokedex";
-  const navigate = useNavigate();
-  const [pokedexPokemons, setPokedexPokemons] = useState([]);
-  const [homePokemons, setHomePokemons] = useState([])
-  const [refreshScreen, setRefreshScreen] = useState(0)
+  const navigate = useNavigate()
 
-  const listPokedex = JSON.parse(localStorage.getItem("listPokedex"));
-  const listPokemonHome = JSON.parse(localStorage.getItem("homePokemon"));
-  console.log(homePokemons)
+  const detailsPage = (id) => {
+      navigate("/details/" + id);
+    };
+  const {pokemomPokedex,setPokemomPokedex} = useContext(GlobalStateContext);
+
+  const removePokemon = (data)=>{
+    const newPokemonPokedex = [...pokemomPokedex];
+    const pokemonIndex = pokemomPokedex.findIndex((item)=>item.id === data.id);
+    newPokemonPokedex.splice(pokemonIndex,1);
+    setPokemomPokedex(newPokemonPokedex);
+
+  }
   
-useEffect( ()=>{
-  if(listPokedex !== null) {
-    setPokedexPokemons(listPokedex);
-  }
-  if(listPokemonHome !== null){
-    return setHomePokemons(listPokemonHome)
-  }
-},[refreshScreen]);
+  console.log(pokemomPokedex);
+  const ListPkemon =
+    pokemomPokedex.map((item) => {
+      return (
+        <CardPokedex key={item.id} namePokemom={item.name} url={item.photo} 
+        buttonRemove={<button onClick={()=>{removePokemon(item)}}>Remover</button>} 
+        buttonView={<button onClick={()=>detailsPage(item.id)}>View Details</button>}/>
+      );
 
+    });
 
-let addListpokeHome;
-const removePokemon = (pokemon,indexPokemon) => {
-  setRefreshScreen(refreshScreen+1);
-  let newList = [...pokedexPokemons];
-  pokedexPokemons.filter( (poke) => {
-    if(poke.name === pokemon.name) {
-      addListpokeHome = [...homePokemons,pokemon]
-      newList.splice(indexPokemon,1);
-      localStorage.setItem("listPokedex",JSON.stringify(newList));
-      setPokedexPokemons(newList);
-    }
-  });
-  setHomePokemons(addListpokeHome);
-  localStorage.setItem("homePokemons",JSON.stringify(addListpokeHome));
-};
-
-return (
-  <>
-  <Headers title={title} />
-        {listPokedex !== null && listPokedex.length > 0  && pokedexPokemons.length < 1 ? listPokedex.map((poke,index)=>{
-        return (
-          <StyleCard
-          key={poke.name}
-          style={poke.types[0].type.name === "grass" ? {backgroundImage: 'linear-gradient(to bottom  , #84ed6829 20%, #49b675 60%, #005221)'} :
-          poke.types[0].type.name === "fire" ? {backgroundImage: 'linear-gradient(to bottom , #ff7f0099 20%, yellow 60%, #ff7f00)'} :
-          poke.types[0].type.name === "water" ? {backgroundImage: 'linear-gradient(to bottom , #85e8ff29 10%, #0d67d2 70%, #4354e9)'} :
-          poke.types[0].type.name === "normal" ? {backgroundImage: 'linear-gradient(to bottom , #fcf5d629 10%, #b5840d 70%, #000)'}: 
-          poke.types[0].type.name === "bug" ? {backgroundImage: 'linear-gradient(to bottom , #78866b29 10%, #fcf5d6 70%, #78866b )'}:{}
-        }></StyleCard>
-        )})
-        :
-        pokedexPokemons.map((poke,index)=>{
-          return (
-            <StyleCard
-            key={poke.name}
-          style={poke.types[0].type.name === "grass" ? {backgroundImage: 'linear-gradient(to bottom  , #84ed6829 20%, #49b675 60%, #005221)'} :
-          poke.types[0].type.name === "fire" ? {backgroundImage: 'linear-gradient(to bottom , #ff7f0099 20%, yellow 60%, #ff7f00)'} :
-          poke.types[0].type.name === "water" ? {backgroundImage: 'linear-gradient(to bottom , #85e8ff29 10%, #0d67d2 70%, #4354e9)'} :
-          poke.types[0].type.name === "normal" ? {backgroundImage: 'linear-gradient(to bottom , #fcf5d629 10%, #b5840d 70%, #000)'}: 
-          poke.types[0].type.name === "bug" ? {backgroundImage: 'linear-gradient(to bottom , #78866b29 10%, #fcf5d6 70%, #78866b )'}:{}
-        }>
-          <button onClick={ ()=> removePokemon(poke,index)}>Remover</button>
-          <button onClick={ ()=> MyRouters.goToDetails(navigate,poke.name)}>Detalhes</button>
-
-        </StyleCard> )})
-}
-</>
+  return (
+    <>
+      <Headers title={title} />
+      <Background>
+        {ListPkemon}
+      </Background>
+    </>
   );
 }
-export default Pokedex;
 
+export default Pokedex;
